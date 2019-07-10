dddc67d87c112d596a209364bd08816aec50ea2f
moe123 <moe@aermob.com>
2019-07-10 15:20:39 -0700

    adding cython layout

--- cython/setup.py
+++ cython/setup.py
@@ -6,8 +6,12 @@
 # Copyright (C) 2019 Moe123. All rights reserved.
 #
 
+import sys, os
+
 from distutils.core import setup
 from Cython.Build   import cythonize
+
+os.environ['CFLAGS'] = '-std=c99 -Wextra -pedantic -Wno-newline-eof -I../ -DMC_DISABLE_REENTRANT=1 -DMC_DISABLE_TGMATH=1'
 
 setup(
 	ext_modules = cythonize(
