a8b111322479b5163f09beeb1bf8069aafe20abb
moe123 <moe@aermob.com>
2019-07-16 17:19:46 -0700

    fixing sqrt1pm1

--- macadam/details/math/mc_sqrt1pm1.h
+++ macadam/details/math/mc_sqrt1pm1.h
@@ -6,9 +6,8 @@
 // Copyright (C) 2019 Moe123. All rights reserved.
 //
 
-#include <macadam/details/mc_target.h>
-#include <macadam/mcconsts.h>
-#include <macadam/mclimits.h>
+#include <macadam/details/math/mc_fabs.h>
+#include <macadam/details/math/mc_sqrt.h>
 
 #ifndef MC_SQRT1PM1_H
 #define MC_SQRT1PM1_H
@@ -17,47 +16,26 @@
 
 static MC_TARGET_INLINE float mc_sqrt1pm1f(float x)
 {
-#	if MC_TARGET_CPP98
-	if (::fabsf(x) > 0.75f) {
-		return ::sqrtf(1.0f + x) - 1.0f;
+	if (mc_fabsf(x) > 0.75f) {
+		return mc_sqrtf(1.0f + x) - 1.0f;
 	}
-	return x / (1.0f + ::sqrtf(1.0f + x));
-#	else
-	if (fabsf(x) > 0.75f) {
-		return sqrtf(1.0f + x) - 1.0f;
-	}
-	return x / (1.0f + sqrtf(1.0f + x));
-#	endif
+	return x / (1.0f + mc_sqrtf(1.0f + x));
 }
 
 static MC_TARGET_INLINE double mc_sqrt1pm1(double x)
 {
-#	if MC_TARGET_CPP98
-	if (::fabs(x) > 0.75) {
-		return ::sqrt(1.0 + x) - 1.0;
+	if (mc_fabs(x) > 0.75) {
+		return mc_sqrt(1.0 + x) - 1.0;
 	}
-	return x / (1.0 + ::sqrt(1.0 + x));
-#	else
-	if (fabs(x) > 0.75) {
-		return sqrt(1.0 + x) - 1.0;
-	}
-	return x / (1.0 + sqrt(1.0 + x));
-#	endif
+	return x / (1.0 + mc_sqrt(1.0 + x));
 }
 
 static MC_TARGET_INLINE long double mc_sqrt1pm1l(long double x)
 {
-#	if MC_TARGET_CPP98
-	if (::fabsl(x) > 0.75L) {
-		return ::sqrtl(1.0L + x) - 1.0L;
+	if (mc_fabsl(x) > 0.75L) {
+		return mc_sqrtl(1.0L + x) - 1.0L;
 	}
-	return x / (1.0L + ::sqrtl(1.0L + x));
-#	else
-	if (fabsl(x) > 0.75L) {
-		return sqrtl(1.0L + x) - 1.0L;
-	}
-	return x / (1.0L + sqrtl(1.0L + x));
-#	endif
+	return x / (1.0L + mc_sqrtl(1.0L + x));
 }
 
 #endif /* !MC_SQRT1PM1_H */
