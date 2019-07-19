4ada6e17dff3549237403ededbea6870ec880140
moe123 <moe@aermob.com>
2019-07-19 11:49:00 -0700

    cython autogen pxd

---
+++ cython/mcmath_pyx.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+#
+# # -*- coding: utf-8, tab-width: 3 -*-
+
+# mcmath_pxd.sh
+#
+# Copyright (C) 2019 Moe123. All rights reserved.
+#
+
+cmd_pushd ()
+{
+	command pushd "$@" > /dev/null
+}
+
+cmd_popd ()
+{
+	command popd "$@" > /dev/null
+}
+
+function main ()
+{ 
+	declare -r path_self=$(cd $(dirname "$0");pwd)
+	declare -r path_base=$(cd $(dirname "$0");cd ..;pwd)
+	if [ "$(uname -s)" == "Darwin" ]
+	then
+		declare -r cmd_sed="gsed"
+	else
+		declare -r cmd_sed="sed"
+	fi
+	cmd_pushd "${path_base}"
+
+	declare -a special
+	special=("fisint" "isfinite" "isinf" "isnan" )
+
+	:> "${path_self}/mcmath.pxd"
+
+	echo "# BOF"                      >> "${path_self}/mcmath.pxd"
+	echo "from libc.stdint cimport *" >> "${path_self}/mcmath.pxd"
+	echo ""                           >> "${path_self}/mcmath.pxd"
+
+	for i in "${!special[@]}"; do 
+		fn="${special[${i}]}"
+		echo "cdef extern from \"<macadam/details/math/mc_${fn}.h>\":" >> "${path_self}/mcmath.pxd"
+		echo "	int mc_${fn}(double x)"                               >> "${path_self}/mcmath.pxd"
+		echo ""                                                        >> "${path_self}/mcmath.pxd"
+	done
+
+	for f in macadam/details/math/*.h; do
+		symbol=$(cat ${f} | grep -E "^MC_TARGET_FUNC double (\w)" | ${cmd_sed} -e "s/MC_TARGET_FUNC /\t/g")
+		if [ ${#symbol} -ne 0 ]
+		then
+			header="cdef extern from \"<${f}>\":"
+			echo "${header}" >> "${path_self}/mcmath.pxd"
+			echo "${symbol}" >> "${path_self}/mcmath.pxd"
+			echo ""          >> "${path_self}/mcmath.pxd"
+		fi
+	done
+
+	printf "# EOF" >> "${path_self}/mcmath.pxd"
+
+	cmd_popd
+}
+
+main "$@"
+
+# EOF
