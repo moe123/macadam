c410a16826c0d95497a2f10c2a804a9388ab21fd
moe123 <moe@aermob.com>
2019-07-24 13:10:22 -0700

    cython panzer scripts

--- cython/mcmath_pxd.sh
+++ cython/mcmath_pxd.sh
@@ -18,20 +18,31 @@ cmd_popd ()
 	command popd "$@" > /dev/null
 }
 
+cmd_exists () {
+	type "$1" &> /dev/null;
+}
+
 function main ()
 { 
 	declare -r path_self=$(cd $(dirname "$0");pwd)
 	declare -r path_base=$(cd $(dirname "$0");cd ..;pwd)
 	if [ "$(uname -s)" == "Darwin" ]
 	then
-		declare -r cmd_sed="gsed"
+		if (cmd_exists gsed); then
+			declare -r cmd_sed="gsed"
+		else
+			echo "Please install gsed cli."
+			echo "https://www.macports.org/ports.php?by=library&substr=gsed"
+			exit 1
+		fi
 	else
 		declare -r cmd_sed="sed"
 	fi
+
 	cmd_pushd "${path_base}"
 
 	declare -a special
-	special=("fisint" "isfinite" "isinf" "isnan" )
+	special=( "fisint" "isfinite" "isinf" "isnan" )
 
 	:> "${path_self}/mcmath.pxd"
 
@@ -42,17 +53,17 @@ function main ()
 	for i in "${!special[@]}"; do 
 		fn="${special[${i}]}"
 		echo "cdef extern from \"<macadam/details/math/mc_${fn}.h>\":" >> "${path_self}/mcmath.pxd"
-		echo "	int mc_${fn}(double x)"                               >> "${path_self}/mcmath.pxd"
+		echo "\tint mc_${fn}(double x)"                                >> "${path_self}/mcmath.pxd"
 		echo ""                                                        >> "${path_self}/mcmath.pxd"
 	done
 
 	for f in macadam/details/math/*.h; do
-		symbol=$(cat ${f} | grep -E "^MC_TARGET_FUNC double (\w)" | ${cmd_sed} -e "s/MC_TARGET_FUNC /\t/g")
-		if [ ${#symbol} -ne 0 ]
+		symbols=$(cat ${f} | grep -E "^MC_TARGET_FUNC double (\w)" | ${cmd_sed} -e "s/MC_TARGET_FUNC /\t/g")
+		if [ ${#symbols} -ne 0 ]
 		then
 			header="cdef extern from \"<${f}>\":"
 			echo "${header}" >> "${path_self}/mcmath.pxd"
-			echo "${symbol}" >> "${path_self}/mcmath.pxd"
+			echo "${symbols}" >> "${path_self}/mcmath.pxd"
 			echo ""          >> "${path_self}/mcmath.pxd"
 		fi
 	done
--- cython/mcmath_pyx.sh
+++ cython/mcmath_pyx.sh
@@ -3,7 +3,7 @@
 #
 # # -*- coding: utf-8, tab-width: 3 -*-
 
-# mcmath_pxd.sh
+# mcmath_pyx.sh
 #
 # Copyright (C) 2019 Moe123. All rights reserved.
 #
@@ -18,46 +18,128 @@ cmd_popd ()
 	command popd "$@" > /dev/null
 }
 
+cmd_exists () {
+	type "$1" &> /dev/null;
+}
+
+function last_index_of ()
+{
+	ifs_current="${IFS}"
+	IFS=$'\n'
+	local str=( $(echo "${1}" | fold -w1) )
+	ifs_current="${IFS}"
+	local index=-1
+	for (( i=${#str[@]}-1 ; i>=0 ; i-- )) ; do
+	 	c=${str[${i}]}
+		if [ "${str[${i}]}" = "${2}" ]; then
+			index=${i}
+			break
+		fi
+	done
+	echo ${index}
+}
+
 function main ()
 { 
 	declare -r path_self=$(cd $(dirname "$0");pwd)
 	declare -r path_base=$(cd $(dirname "$0");cd ..;pwd)
 	if [ "$(uname -s)" == "Darwin" ]
 	then
-		declare -r cmd_sed="gsed"
+		if (cmd_exists gsed); then
+			declare -r cmd_sed="gsed"
+		else
+			echo "Please install gsed cli."
+			echo "https://www.macports.org/ports.php?by=library&substr=gsed"
+			exit 1
+		fi
 	else
 		declare -r cmd_sed="sed"
 	fi
+
 	cmd_pushd "${path_base}"
 
-	declare -a special
-	special=("fisint" "isfinite" "isinf" "isnan" )
+	:> "${path_self}/mcmath.pyx"
 
-	:> "${path_self}/mcmath.pxd"
+	echo "# BOF"          >> "${path_self}/mcmath.pyx"
+	echo "cimport mcmath" >> "${path_self}/mcmath.pyx"
+	echo ""               >> "${path_self}/mcmath.pyx"
 
-	echo "# BOF"                      >> "${path_self}/mcmath.pxd"
-	echo "from libc.stdint cimport *" >> "${path_self}/mcmath.pxd"
-	echo ""                           >> "${path_self}/mcmath.pxd"
+	declare -a symbols
 
-	for i in "${!special[@]}"; do 
-		fn="${special[${i}]}"
-		echo "cdef extern from \"<macadam/details/math/mc_${fn}.h>\":" >> "${path_self}/mcmath.pxd"
-		echo "	int mc_${fn}(double x)"                               >> "${path_self}/mcmath.pxd"
-		echo ""                                                        >> "${path_self}/mcmath.pxd"
-	done
+	ifs_current="${IFS}"
+	IFS=$'\n'
+	symbols=( $(cat "${path_self}/mcmath.pxd" | grep -E "^\t(double|int) (\w)" | ${cmd_sed} -e "s/\t//g; s/[*]/\\\*/g") )
+	ifs_current="${IFS}"
 
-	for f in macadam/details/math/*.h; do
-		symbol=$(cat ${f} | grep -E "^MC_TARGET_FUNC double (\w)" | ${cmd_sed} -e "s/MC_TARGET_FUNC /\t/g")
-		if [ ${#symbol} -ne 0 ]
-		then
-			header="cdef extern from \"<${f}>\":"
-			echo "${header}" >> "${path_self}/mcmath.pxd"
-			echo "${symbol}" >> "${path_self}/mcmath.pxd"
-			echo ""          >> "${path_self}/mcmath.pxd"
+	IFS=$'|'
+	for i in "${!symbols[@]}"
+	do
+		declare -a entities
+		ifs_current="${IFS}"
+		IFS=$'|'
+		entities=( $(echo "${symbols[${i}]}" | ${cmd_sed} -n "s/^\(.*\) mc_\(.*\)(\(.*\))/\1\|\2\|\3/p") )
+		IFS="${ifs_current}"
+
+		declare -a cparameter_pairs
+
+		declare call_return="${entities[0]}"
+		declare call_symbol="${entities[1]}"
+
+		ifs_current="${IFS}"
+		IFS=$','
+		cparameter_pairs=( ${entities[2]} )
+		IFS="${ifs_current}"
+
+		unset entities
+
+		if [ "${call_symbol}" = "modf" ]; then
+			echo "def ${call_symbol}(x):"                                        >> "${path_self}/mcmath.pyx"
+			echo "\tcdef double p0 = x"                                          >> "${path_self}/mcmath.pyx"
+			echo "\tcdef double p1 = 0"                                          >> "${path_self}/mcmath.pyx"
+			echo "\tcdef ${call_return} r0 = mcmath.mc_${call_symbol}(p0, &p1)"  >> "${path_self}/mcmath.pyx"
+			echo "\treturn r0, p1"                                               >> "${path_self}/mcmath.pyx"
+			echo ""                                                              >> "${path_self}/mcmath.pyx"
+		else
+			declare -a cparameter_types
+			declare -a cparameter_names
+			tuple=""
+			for j in "${!cparameter_pairs[@]}"; do
+				cparameter_pair=$(echo "${cparameter_pairs[${j}]}" | awk '{$1=$1};1')
+				index=$(last_index_of "${cparameter_pair}" ' ')
+				if [[ ${index} -ge 0 ]]; then
+					cparameter_types[${j}]="${cparameter_pair:0:${index}}"
+					cparameter_names[${j}]="${cparameter_pair:${index} + 1}"
+					if [[ ${j} -eq 0 ]]; then
+						tuple="${cparameter_names[${j}]}"
+					else
+						tuple=$(echo "${tuple}", "${cparameter_names[${j}]}")
+					fi
+				fi
+				unset cparameter_pair
+				unset index
+			done
+			echo "def ${call_symbol}(${tuple}):"                                         >> "${path_self}/mcmath.pyx"
+			tuple=""
+			for k in "${!cparameter_types[@]}"; do
+				echo "\tcdef ${cparameter_types[${k}]} p${k} = ${cparameter_names[${k}]}" >> "${path_self}/mcmath.pyx"
+				if [[ ${k} -eq 0 ]]; then
+					tuple="p${k}"
+				else
+					tuple=$(echo "${tuple}", "p${k}")
+				fi
+			done
+			echo "\tcdef ${call_return} r0 = mcmath.mc_${call_symbol}(${tuple})"         >> "${path_self}/mcmath.pyx"
+			echo "\treturn r0"                                                           >> "${path_self}/mcmath.pyx"
+			echo ""                                                                      >> "${path_self}/mcmath.pyx"
+			unset cparameter_types
+			unset cparameter_names
 		fi
+		unset call_return
+		unset call_symbol
+		unset cparameter_pairs
 	done
 
-	printf "# EOF" >> "${path_self}/mcmath.pxd"
+	printf "# EOF" >> "${path_self}/mcmath.pyx"
 
 	cmd_popd
 }
