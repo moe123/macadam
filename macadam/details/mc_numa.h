75a7e816cf909dcd42371c5dc2a31a11525f5aab
moe123 <moe@aermob.com>
2019-11-15 16:33:01 -0800

    numa sumtwo1xn

--- macadam/details/mc_numa.h
+++ macadam/details/mc_numa.h
@@ -125,6 +125,7 @@
 #	include <macadam/details/numa/mc_ssqrmx1.h>
 #	include <macadam/details/numa/mc_sum1xn.h>
 #	include <macadam/details/numa/mc_sumsq1xn.h>
+#	include <macadam/details/numa/mc_sumtwo1xn.h>
 #	include <macadam/details/numa/mc_trace2x2.h>
 #	include <macadam/details/numa/mc_trace3x3.h>
 #	include <macadam/details/numa/mc_tracemxn.h>
---
+++ macadam/details/numa/mc_sumtwo1xn.h
@@ -0,0 +1,60 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+// mc_sumtwo1xn.h
+//
+// Copyright (C) 2019 Moe123. All rights reserved.
+//
+
+#include <macadam/details/math/mc_twosum.h>
+
+#ifndef MC_SUMTWO1XN_H
+#define MC_SUMTWO1XN_H
+
+#pragma mark - mc_sumtwo1xn -
+
+MC_TARGET_FUNC float mc_sumtwo1xnf(int n, const float * x)
+{
+	int i   = 1;
+	float e = 0.0f, s = 0.0f, y;
+	if (n > 0) {
+		s = x[0];
+		for (; i < n; i++) {
+			mc_twosumf(s, x[i], &s, &y);
+			e = e + y;
+		}
+	}
+	return s + e;
+}
+
+MC_TARGET_FUNC double mc_sumtwo1xn(int n, const double * x)
+{
+	int i    = 1;
+	double e = 0.0, s = 0.0, y;
+	if (n > 0) {
+		s = x[0];
+		for (; i < n; i++) {
+			mc_twosum(s, x[i], &s, &y);
+			e = e + y;
+		}
+	}
+	return s + e;
+}
+
+MC_TARGET_FUNC long double mc_sumtwo1xnl(int n, const long double * x)
+{
+	int i         = 1;
+	long double e = 0.0L, s = 0.0L, y;
+	if (n > 0) {
+		s = x[0];
+		for (; i < n; i++) {
+			mc_twosuml(s, x[i], &s, &y);
+			e = e + y;
+		}
+	}
+	return s + e;
+}
+
+#endif /* !MC_SUMTWO1XN_H */
+
+/* EOF */
