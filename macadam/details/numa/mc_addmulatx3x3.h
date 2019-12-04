0b072cce8ebaf6a0f1750fa786351b919f573d6a
moe123 <moe@aermob.com>
2019-12-04 15:43:58 -0800

    numa fixing mess

--- macadam/details/numa/mc_addmulatx3x3.h
+++ macadam/details/numa/mc_addmulatx3x3.h
@@ -29,23 +29,23 @@ MC_TARGET_FUNC void mc_addmulatx3x3f(float * b, const float a[9], const float x[
 	b[1] = b[1] + (a[7] * x2);
 
 	b[2] = b[2] + (a[2] * x0);
-	b[2] = b[2] + (a[4] * x1);
-	b[2] = b[2] + (a[5] * x2);
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 MC_TARGET_FUNC void mc_addmulatx3x3ff(double * b, const float a[9], const float x[3])
 {
 //!# b=b + a'*x
 	b[0] = mc_cast(double, b[0]) + (mc_cast(double, a[0]) * mc_cast(double, x[0]));
-	b[0] = mc_cast(double, b[0]) + (mc_cast(double, a[1]) * mc_cast(double, x[1]));
-	b[0] = mc_cast(double, b[0]) + (mc_cast(double, a[2]) * mc_cast(double, x[2]));
+	b[0] = mc_cast(double, b[0]) + (mc_cast(double, a[3]) * mc_cast(double, x[1]));
+	b[0] = mc_cast(double, b[0]) + (mc_cast(double, a[6]) * mc_cast(double, x[2]));
 
-	b[1] = mc_cast(double, b[1]) + (mc_cast(double, a[3]) * mc_cast(double, x[0]));
+	b[1] = mc_cast(double, b[1]) + (mc_cast(double, a[1]) * mc_cast(double, x[0]));
 	b[1] = mc_cast(double, b[1]) + (mc_cast(double, a[4]) * mc_cast(double, x[1]));
-	b[1] = mc_cast(double, b[1]) + (mc_cast(double, a[5]) * mc_cast(double, x[2]));
+	b[1] = mc_cast(double, b[1]) + (mc_cast(double, a[7]) * mc_cast(double, x[2]));
 
-	b[2] = mc_cast(double, b[2]) + (mc_cast(double, a[6]) * mc_cast(double, x[0]));
-	b[2] = mc_cast(double, b[2]) + (mc_cast(double, a[7]) * mc_cast(double, x[1]));
+	b[2] = mc_cast(double, b[2]) + (mc_cast(double, a[2]) * mc_cast(double, x[0]));
+	b[2] = mc_cast(double, b[2]) + (mc_cast(double, a[5]) * mc_cast(double, x[1]));
 	b[2] = mc_cast(double, b[2]) + (mc_cast(double, a[8]) * mc_cast(double, x[2]));
 }
 
@@ -65,8 +65,8 @@ MC_TARGET_FUNC void mc_addmulatx3x3(double * b, const double a[9], const double 
 	b[1] = b[1] + (a[7] * x2);
 
 	b[2] = b[2] + (a[2] * x0);
-	b[2] = b[2] + (a[4] * x1);
-	b[2] = b[2] + (a[5] * x2);
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 MC_TARGET_FUNC void mc_addmulatx3x3l(long double * b, const long double a[9], const long double x[3])
@@ -85,8 +85,8 @@ MC_TARGET_FUNC void mc_addmulatx3x3l(long double * b, const long double a[9], co
 	b[1] = b[1] + (a[7] * x2);
 
 	b[2] = b[2] + (a[2] * x0);
-	b[2] = b[2] + (a[4] * x1);
-	b[2] = b[2] + (a[5] * x2);
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 #endif /* !MC_ADDMULAX3X3_H */
--- macadam/details/numa/mc_mulatx3x3.h
+++ macadam/details/numa/mc_mulatx3x3.h
@@ -20,17 +20,17 @@ MC_TARGET_FUNC void mc_mulatx3x3f(float * b, const float a[9], const float x[3])
 	const float x1 = x[1];
 	const float x2 = x[2];
 
-	b[0] = (a[0] * x0);
-	b[0] = (a[3] * x1);
-	b[0] = (a[6] * x2);
+	b[0] = a[0] * x0;
+	b[0] = b[0] + (a[3] * x1);
+	b[0] = b[0] + (a[6] * x2);
 
-	b[1] = (a[1] * x0);
-	b[1] = (a[4] * x1);
-	b[1] = (a[7] * x2);
+	b[1] = a[1] * x0;
+	b[1] = b[1] + (a[4] * x1);
+	b[1] = b[1] + (a[7] * x2);
 
-	b[2] = (a[2] * x0);
-	b[2] = (a[4] * x1);
-	b[2] = (a[5] * x2);
+	b[2] = a[2] * x0;
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 MC_TARGET_FUNC void mc_mulatx3x3ff(double * b, const float a[9], const float x[3])
@@ -47,17 +47,17 @@ MC_TARGET_FUNC void mc_mulatx3x3(double * b, const double a[9], const double x[3
 	const double x1 = x[1];
 	const double x2 = x[2];
 
-	b[0] = (a[0] * x0);
-	b[0] = (a[3] * x1);
-	b[0] = (a[6] * x2);
+	b[0] = a[0] * x0;
+	b[0] = b[0] + (a[3] * x1);
+	b[0] = b[0] + (a[6] * x2);
 
-	b[1] = (a[1] * x0);
-	b[1] = (a[4] * x1);
-	b[1] = (a[7] * x2);
+	b[1] = a[1] * x0;
+	b[1] = b[1] + (a[4] * x1);
+	b[1] = b[1] + (a[7] * x2);
 
-	b[2] = (a[2] * x0);
-	b[2] = (a[4] * x1);
-	b[2] = (a[5] * x2);
+	b[2] = a[2] * x0;
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 MC_TARGET_FUNC void mc_mulatx3x3l(long double * b, const long double a[9], const long double x[3])
@@ -67,17 +67,17 @@ MC_TARGET_FUNC void mc_mulatx3x3l(long double * b, const long double a[9], const
 	const long double x1 = x[1];
 	const long double x2 = x[2];
 
-	b[0] = (a[0] * x0);
-	b[0] = (a[3] * x1);
-	b[0] = (a[6] * x2);
+	b[0] = a[0] * x0;
+	b[0] = b[0] + (a[3] * x1);
+	b[0] = b[0] + (a[6] * x2);
 
-	b[1] = (a[1] * x0);
-	b[1] = (a[4] * x1);
-	b[1] = (a[7] * x2);
+	b[1] = a[1] * x0;
+	b[1] = b[1] + (a[4] * x1);
+	b[1] = b[1] + (a[7] * x2);
 
-	b[2] = (a[2] * x0);
-	b[2] = (a[4] * x1);
-	b[2] = (a[5] * x2);
+	b[2] = a[2] * x0;
+	b[2] = b[2] + (a[5] * x1);
+	b[2] = b[2] + (a[8] * x2);
 }
 
 #endif /* !MC_ADDMULAX3X3_H */
