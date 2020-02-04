789d04d06a73261099b43affa4649e9ba0dfe226
moe123 <moe@aermob.com>
2020-02-03 17:23:52 -0800

    numa fixing blax and n dim

--- macadam/details/numa/mc_vander1xn.h
+++ macadam/details/numa/mc_vander1xn.h
@@ -16,14 +16,15 @@
 MC_TARGET_FUNC void mc_vander1xnf(int n, int d, float * restrict a, const float * x, int f)
 {
 //!# a[n * (d + 1)] and x[n].
-//!# f=0: ascending powers (default).
-//!# f=1: descending powers (matlab and octave).
+//!# Returns the Vandermonde matrix A such that its columns are powers of the vector X.
+//!# The length of vector X representing the m dimension and d=degree + 1 the n dimension
+//!# of the output matrix A.
 	int i = 0, j, k;
 	float w, v, e;
 	for (; i < n; i++) {
 		w = 1.0f;
 		for (j = 0; j < d + 1; j++) {
-			k    = (f == 1) ? ((n * i) + (d - j)) :  ((n * i) + j);
+			k    = (f == 1) ? (((d + 1) * i) + (d - j)) :  (((d + 1) * i) + j);
 			a[k] = w;
 			mc_twoproductf(w, x[i], &v, &e);
 			w    = v + e;
@@ -34,14 +35,15 @@ MC_TARGET_FUNC void mc_vander1xnf(int n, int d, float * restrict a, const float 
 MC_TARGET_FUNC void mc_vander1xnff(int n, int d, double * a, const float * x, int f)
 {
 //!# a[n * (d + 1)] and x[n].
-//!# f=0: ascending powers (default).
-//!# f=1: descending powers (matlab and octave).
+//!# Returns the Vandermonde matrix A such that its columns are powers of the vector X.
+//!# The length of vector X representing the m dimension and d=degree + 1 the n dimension
+//!# of the output matrix A.
 	int i = 0, j, k;
 	double w, v, e;
 	for (; i < n; i++) {
 		w = 1.0;
 		for (j = 0; j < d + 1; j++) {
-			k    = (f == 1) ? ((n * i) + (d - j)) :  ((n * i) + j);
+			k    = (f == 1) ? (((d + 1) * i) + (d - j)) :  (((d + 1) * i) + j);
 			a[k] = w;
 			mc_twoproduct(w, mc_cast(double, x[i]), &v, &e);
 			w    = v + e;
@@ -52,14 +54,15 @@ MC_TARGET_FUNC void mc_vander1xnff(int n, int d, double * a, const float * x, in
 MC_TARGET_FUNC void mc_vander1xn(int n, int d, double * restrict a, const double * x, int f)
 {
 //!# a[n * (d + 1)] and x[n].
-//!# f=0: ascending powers (default).
-//!# f=1: descending powers (matlab and octave).
+//!# Returns the Vandermonde matrix A such that its columns are powers of the vector X.
+//!# The length of vector X representing the m dimension and d=degree + 1 the n dimension
+//!# of the output matrix A.
 	int i = 0, j, k;
 	double w, v, e;
 	for (; i < n; i++) {
 		w = 1.0;
 		for (j = 0; j < d + 1; j++) {
-			k    = (f == 1) ? ((n * i) + (d - j)) :  ((n * i) + j);
+			k    = (f == 1) ? (((d + 1) * i) + (d - j)) :  (((d + 1) * i) + j);
 			a[k] = w;
 			mc_twoproduct(w, x[i], &v, &e);
 			w    = v + e;
@@ -70,6 +73,9 @@ MC_TARGET_FUNC void mc_vander1xn(int n, int d, double * restrict a, const double
 MC_TARGET_FUNC void mc_vander1xnl(int n, int d, long double * restrict a, const long double * x, int f)
 {
 //!# a[n * (d + 1)] and x[n].
+//!# Returns the Vandermonde matrix A such that its columns are powers of the vector X.
+//!# The length of vector X representing the m dimension and d=degree + 1 the n dimension
+//!# of the output matrix A.
 //!# f=0: ascending powers (default).
 //!# f=1: descending powers (matlab and octave).
 	int i = 0, j, k;
@@ -77,7 +83,7 @@ MC_TARGET_FUNC void mc_vander1xnl(int n, int d, long double * restrict a, const 
 	for (; i < n; i++) {
 		w = 1.0L;
 		for (j = 0; j < d + 1; j++) {
-			k    = (f == 1) ? ((n * i) + (d - j)) :  ((n * i) + j);
+			k    = (f == 1) ? (((d + 1) * i) + (d - j)) :  (((d + 1) * i) + j);
 			a[k] = w;
 			mc_twoproductl(w, x[i], &v, &e);
 			w    = v + e;
