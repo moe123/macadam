dc80d461a574827418626c85e4888611ae7fb15e
moe123 <moe@aermob.com>
2020-02-05 16:48:29 -0800

    numa + utilities

--- macadam/details/numa/mc_argsort1xn.h
+++ macadam/details/numa/mc_argsort1xn.h
@@ -12,51 +12,39 @@
 #ifndef MC_ARGSORT1XN_H
 #define MC_ARGSORT1XN_H
 
+#pragma mark - mc_argsort1xn_type -
+
+#	define mc_argsort1xn_type(integer_type, n, x, k, f)                                                                              \
+	mc_scope_begin                                                                                                                   \
+		int __mc_argsort1xn_i = 0, __mc_argsort1xn_j;                                                                                 \
+		integer_type __mc_argsort1xn_w;                                                                                               \
+		for (; __mc_argsort1xn_i < n; __mc_argsort1xn_i++) {                                                                          \
+			k[__mc_argsort1xn_i] = mc_cast(integer_type, __mc_argsort1xn_i);                                                           \
+		}                                                                                                                             \
+		for (__mc_argsort1xn_i = 0; __mc_argsort1xn_i < n; __mc_argsort1xn_i++) {                                                     \
+			for (__mc_argsort1xn_j = 1; __mc_argsort1xn_j < (n - __mc_argsort1xn_i); __mc_argsort1xn_j++) {                            \
+				if ((f == 1) ? (x[__mc_argsort1xn_j - 1] < x[__mc_argsort1xn_j]) : (x[__mc_argsort1xn_j - 1] > x[__mc_argsort1xn_j])) { \
+					mcswap_var(__mc_argsort1xn_w, k[__mc_argsort1xn_j - 1], k[__mc_argsort1xn_j]);                                       \
+				}                                                                                                                       \
+			}                                                                                                                          \
+		}                                                                                                                             \
+	mc_scope_end
+
 #pragma mark - mc_argsort1xn -
 
 MC_TARGET_FUNC void mc_argsort1xnf(int n, const float * x, int * k, int f)
 {
-	int i = 0, j, w;
-	for (; i < n; i++) {
-		k[i] = i;
-	}
-	for (i = 0; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (x[j - 1] < x[j]) : (x[j - 1] > x[j])) {
-				mcswap_var(w, k[j - 1], k[j]);
-			}
-		}
-	}
+	mc_argsort1xn_type(int, n, x, k, f);
 }
 
 MC_TARGET_FUNC void mc_argsort1xn(int n, const double * x, int * k, int f)
 {
-	int i = 0, j, w;
-	for (; i < n; i++) {
-		k[i] = i;
-	}
-	for (i = 0; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (x[j - 1] < x[j]) : (x[j - 1] > x[j])) {
-				mcswap_var(w, k[j - 1], k[j]);
-			}
-		}
-	}
+	mc_argsort1xn_type(int, n, x, k, f);
 }
 
 MC_TARGET_FUNC void mc_argsort1xnl(int n, const long double * x, int * k, int f)
 {
-	int i = 0, j, w;
-	for (; i < n; i++) {
-		k[i] = i;
-	}
-	for (i = 0; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (x[j - 1] < x[j]) : (x[j - 1] > x[j])) {
-				mcswap_var(w, k[j - 1], k[j]);
-			}
-		}
-	}
+	mc_argsort1xn_type(int, n, x, k, f);
 }
 
 #endif /* !MC_ARGSORT1XN_H */
--- macadam/details/numa/mc_sort1xn.h
+++ macadam/details/numa/mc_sort1xn.h
@@ -12,68 +12,51 @@
 #ifndef MC_SORT1XN_H
 #define MC_SORT1XN_H
 
+#pragma mark - mc_sort1xn_type -
+
+#	define mc_sort1xn_type(type, n, x, f)                                                                                                    \
+	mc_scope_begin                                                                                                                           \
+		int __mc_sort1xn_type_i = 0, __mc_sort1xn_type_j;                                                                                     \
+		type __mc_sort1xn_type_w;                                                                                                             \
+		for (; __mc_sort1xn_type_i < n; __mc_sort1xn_type_i++) {                                                                              \
+			for (__mc_sort1xn_type_j = 1; __mc_sort1xn_type_j < (n - __mc_sort1xn_type_i); __mc_sort1xn_type_j++) {                            \
+				if ((f == 1) ? (x[__mc_sort1xn_type_j - 1] < x[__mc_sort1xn_type_j]) : (x[__mc_sort1xn_type_j - 1] > x[__mc_sort1xn_type_j])) { \
+					mcswap_var(__mc_sort1xn_type_w, x[__mc_sort1xn_type_j - 1], x[__mc_sort1xn_type_j]);                                         \
+				}                                                                                                                               \
+			}                                                                                                                                  \
+		}                                                                                                                                     \
+	mc_scope_end
+
 #pragma mark - mc_sort1xn -
 
 MC_TARGET_FUNC void mc_sort1xnf(int n, float * y, const float * x, int f)
 {
-	int i = 0, j;
-	float w;
 	if (x != y) {
 		mc_copy1xnf(n, y, x);
 	}
-	for (; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (y[j - 1] < y[j]) : (y[j - 1] > y[j])) {
-				mcswap_var(w, y[j - 1], y[j]);
-			}
-		}
-	}
+	mc_sort1xn_type(float, n, y, f);
 }
 
 MC_TARGET_FUNC void mc_sort1xnff(int n, double * y, const float * x, int f)
 {
-	int i = 0, j;
-	double w;
 	mc_copy1xnff(n, y, x);
-	for (; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (y[j - 1] < y[j]) : (y[j - 1] > y[j])) {
-				mcswap_var(w, y[j - 1], y[j]);
-			}
-		}
-	}
+	mc_sort1xn_type(double, n, y, f);
 }
 
 MC_TARGET_FUNC void mc_sort1xn(int n, double * y, const double * x, int f)
 {
-	int i = 0, j;
-	double w;
 	if (x != y) {
 		mc_copy1xn(n, y, x);
 	}
-	for (; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (y[j - 1] < y[j]) : (y[j - 1] > y[j])) {
-				mcswap_var(w, y[j - 1], y[j]);
-			}
-		}
-	}
+	mc_sort1xn_type(double, n, y, f);
 }
 
 MC_TARGET_FUNC void mc_sort1xnl(int n, long double * y, const long double * x, int f)
 {
-	int i = 0, j;
-	long double w;
 	if (x != y) {
 		mc_copy1xnl(n, y, x);
 	}
-	for (; i < n; i++) {
-		for (j = 1; j < (n - i); j++) {
-			if ((f == 1) ? (y[j - 1] < y[j]) : (y[j - 1] > y[j])) {
-				mcswap_var(w, y[j - 1], y[j]);
-			}
-		}
-	}
+	mc_sort1xn_type(long double, n, y, f);
 }
 
 #endif /* !MC_SORT1XN_H */
