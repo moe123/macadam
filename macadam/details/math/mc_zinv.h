133f0842823768806393dd977504a692ad585da7
moe123 <moe@aermob.com>
2021-03-15 13:37:08 -0700

    make true recip

--- macadam/details/math/mc_zinv.h
+++ macadam/details/math/mc_zinv.h
@@ -6,6 +6,7 @@
 // Copyright (C) 2019-2021 Moe123. All rights reserved.
 //
 
+#include <macadam/details/math/mc_copysign.h>
 #include <macadam/details/math/mc_zdiv.h>
 
 #ifndef MC_ZINV_H
@@ -16,19 +17,43 @@
 MC_TARGET_PROC void mc_zinvf(float * c_r, float * c_i
 	, const float a_r, const float a_i
 ) {
-	mc_zdivf(c_r, c_i, 1.0f, 0.0f, a_r, a_i);
+	const float d = mc_zabsf(a_r, a_i);
+	const float r = d != 0.0f ? 1.0f / d : MCK_NAN;
+	if (!mc_isnan(r)) {
+		*c_r = +(a_r * r) * r;
+		*c_i = -(a_i * r) * r;
+	} else {
+		*c_r = 0.0f;
+		*c_i = -mc_copysignf(0.0f, a_i);
+	}
 }
 
 MC_TARGET_PROC void mc_zinv(double * c_r, double * c_i
 	, const double a_r, const double a_i
 ) {
-	mc_zdiv(c_r, c_i, 1.0, 0.0, a_r, a_i);
+	const double d = mc_zabs(a_r, a_i);
+	const double r = d != 0.0 ? 1.0 / d : MCK_NAN;
+	if (!mc_isnan(r)) {
+		*c_r = +(a_r * r) * r;
+		*c_i = -(a_i * r) * r;
+	} else {
+		*c_r = 0.0;
+		*c_i = -mc_copysign(0.0, a_i);
+	}
 }
 
 MC_TARGET_PROC void mc_zinvl(long double * c_r, long double * c_i
 	, const long double a_r, const long double a_i
 ) {
-	mc_zdivl(c_r, c_i, 1.0L, 0.0L, a_r, a_i);
+	const long double d = mc_zabsl(a_r, a_i);
+	const long double r = d != 0.0L ? 1.0L / d : MCK_NAN;
+	if (!mc_isnan(r)) {
+		*c_r = +(a_r * r) * r;
+		*c_i = -(a_i * r) * r;
+	} else {
+		*c_r = 0.0L;
+		*c_i = -mc_copysignl(0.0L, a_i);
+	}
 }
 
 #endif /* !MC_ZINV_H */
