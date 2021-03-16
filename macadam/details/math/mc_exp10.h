71bcc8fbeccec074c1902597000e52f7d1eedbc7
moe123 <moe@aermob.com>
2021-03-15 17:17:13 -0700

    math using pow, broken native exp10

--- macadam/details/math/mc_exp10.h
+++ macadam/details/math/mc_exp10.h
@@ -6,9 +6,7 @@
 // Copyright (C) 2019-2021 Moe123. All rights reserved.
 //
 
-#include <macadam/details/mc_target.h>
-#include <macadam/mcconsts.h>
-#include <macadam/mclimits.h>
+#include <macadam/details/math/mc_pow.h>
 
 #ifndef MC_EXP10_H
 #define MC_EXP10_H
@@ -17,69 +15,17 @@
 
 MC_TARGET_FUNC float mc_exp10f(const float x)
 {
-#	if MC_TARGET_CPP98
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return ::exp10f(x);
-#	elif MC_TARGET_APPLEXM
-	return ::__exp10f(x);
-#	else
-	return ::expf(MCK_KF(MCK_LOGE10) * x);
-#	endif
-#	else
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return exp10f(x);
-#	elif MC_TARGET_APPLEXM
-	return __exp10f(x);
-#	else
-	return expf(MCK_KF(MCK_LOGE10) * x);
-#	endif
-#	endif
+	return mc_powf(10.0f, x);
 }
 
 MC_TARGET_FUNC double mc_exp10(const double x)
 {
-#	if MC_TARGET_CPP98
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return ::exp10(x);
-#	else
-	return ::exp(MCK_K(MCK_LOGE10) * x);
-#	endif
-#	else
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return exp10(x);
-#	else
-	return exp(MCK_K(MCK_LOGE10) * x);
-#	endif
-#	endif
+	return mc_pow(10.0, x);
 }
 
 MC_TARGET_FUNC long double mc_exp10l(const long double x)
 {
-#	if MC_TARGET_CPP98
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return ::exp10l(x);
-#	else
-#	if MC_TARGET_CPP17 && defined(M_LN10l)
-	return ::expl(M_LN10l * x);
-#	elif MC_TARGET_CPP17 && defined(M_LN10L)
-	return ::expl(M_LN10L * x);
-#	else
-	return ::expl(MCK_KL(MCK_LOGE10) * x);
-#	endif
-#	endif
-#	else
-#	if (defined(__GLIBC__) || defined(_GNU_SOURCE))
-	return exp10l(x);
-#	else
-#	if MC_TARGET_C99 && defined(M_LN10l)
-	return expl(M_LN10l * x);
-#	elif MC_TARGET_C99 && defined(M_LN10L)
-	return expl(M_LN10L * x);
-#	else
-	return expl(MCK_KL(MCK_LOGE10) * x);
-#	endif
-#	endif
-#	endif
+	return mc_powl(10.0L, x);
 }
 
 #endif /* !MC_EXP10_H */
