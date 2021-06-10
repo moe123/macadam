c9027d0ec4a71a6c9739657e79864bde93f8b202
moe123 <moe@aermob.com>
2021-06-09 20:17:51 -0700

    mc_rsqrt strong bugs fixes

--- macadam/details/math/mc_rsqrt.h
+++ macadam/details/math/mc_rsqrt.h
@@ -6,9 +6,8 @@
 // Copyright (C) 2019-2021 Moe123. All rights reserved.
 //
 
-#include <macadam/details/mc_target.h>
-#include <macadam/mcconsts.h>
-#include <macadam/mclimits.h>
+#include <macadam/details/math/mc_isinf.h>
+#include <macadam/details/math/mc_isnan.h>
 
 #ifndef MC_RSQRT_H
 #define MC_RSQRT_H
@@ -17,12 +16,23 @@
 
 MC_TARGET_FUNC float mc_rsqrtf(const float x)
 {
+	if (mc_isnan(x) || mc_isinf(x)) {
+		return x;
+	}
+	if (x <= 0.0f) {
+		return MCK_INF;
+	}
 #	if MC_TARGET_HAVE_SSE
-	__m128 y  = _mm_set_ss(x);
-	__m128 e  = _mm_rsqrt_ss(y);
-	__m128 e3 = _mm_mul_ss(_mm_mul_ss(e, e), e);
-	__m128 h  = _mm_set_ss(0.5f);
-	return _mm_cvtss_f32(_mm_add_ss(e, _mm_mul_ss(h, _mm_sub_ss(e, _mm_mul_ss(y, e3)))));
+	//__m128 y  = _mm_set_ss(x);
+	//__m128 e  = _mm_rsqrt_ss(y);
+	//__m128 e3 = _mm_mul_ss(_mm_mul_ss(e, e), e);
+	//__m128 h  = _mm_set_ss(0.5f);
+	//return _mm_cvtss_f32(_mm_add_ss(e, _mm_mul_ss(h, _mm_sub_ss(e, _mm_mul_ss(y, e3)))));
+	const float x2 = x * 0.5f;
+	const float f3 = 1.5f;
+	float y;
+	_mm_store_ss(&y, _mm_rsqrt_ss(_mm_set_ps1(x)));
+	return y * (f3 - (x2 * y * y));
 #	elif MC_TARGET_HAVE_NEON
 	float32x2_t y = vdup_n_f32(x);
 	float32x2_t e = vrsqrte_f32(y);
@@ -30,76 +40,55 @@ MC_TARGET_FUNC float mc_rsqrtf(const float x)
 	e             = vmul_f32(e, vrsqrts_f32(y, vmul_f32(e, e)));
 	return vget_lane_f32(e, 0);
 #	elif MC_TARGET_C99 || MC_TARGET_CPP11
-	float y   = x;
-	float x_2 = y * 0.5f;
-	int32_t i = *(int32_t *)&y;
-	i         = 0x5F375A86 - (i >> 1);
-	y         = *(float *)&i;
-	y         = y * (1.5f - (x_2 * y * y));
-	y         = y * (1.5f - (x_2 * y * y));
-	return y;
+	const float x2                      = x * 0.5f;
+	const float f3                      = 1.5f;
+	union { float f; uint32_t i; } conv = { .f = x };
+	//conv.i                              = 0x5F3759DF - (conv.i >> 1);
+	conv.i                              = 0x5F375A86 - (conv.i >> 1);
+	conv.f                              = conv.f * (f3 - (x2 * conv.f * conv.f));
+	return conv.f;
 #	else
-	float g = 2.0f / x;
-	float h = g;
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	g = g * (1.5f - (h) * g * g);
-	return g;
+	return 1.0f / x;
 #	endif
 } 
 
 MC_TARGET_FUNC double mc_rsqrt(const double x)
 {
+	if (mc_isnan(x) || mc_isinf(x)) {
+		return x;
+	}
+	if (x <= 0.0) {
+		return MCK_INF;
+	}
 #	if MC_TARGET_HAVE_SSE
 		double r;
-		__m128d v = _mm_set_pd(x, 0);
-		v         = _mm_sqrt_pd(v);
-		v         = _mm_div_pd(_mm_set_pd(1.0, 0), v);
+		const __m128d d = _mm_set_pd(0.0, 1.0);
+		__m128d v       = _mm_set_pd(0.0, x);
+		v               = _mm_sqrt_pd(v);
+		v               = _mm_div_pd(d, v);
 		_mm_storel_pd(&r, v);
 		return r;
 #	elif MC_TARGET_C99 || MC_TARGET_CPP11
-	double y   = x;
-	double x_2 = y * 0.5;
-	int64_t i  = *(int64_t *)&y;
-	i          = 0x5FE6EB50C7B537A9 - (i >> 1);
-	y          = *(double *)&i;
-	y          = y * (1.5 - (x_2 * y * y));
-	y          = y * (1.5 - (x_2 * y * y));
-	return y;
+	const double x2                      = x * 0.5;
+	const double f3                      = 1.5;
+	union { double d; uint64_t i; } conv = { .d = x };
+	conv.i                               = 0x5FE6EB50C7B537A9 - (conv.i >> 1);
+	conv.d                               = conv.d * (f3 - (x2 * conv.d * conv.d));
+	return conv.d;
 #	else
-	double g = 2.0 / x;
-	double h = g;
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	g = g * (1.5 - (h) * g * g);
-	return g;
+	return 1.0 / x;
 #	endif
 }
 
 MC_TARGET_FUNC long double mc_rsqrtl(const long double x)
 {
-#	if MC_TARGET_HAVE_SSE
-	return mc_cast(long double, mc_rsqrt(mc_cast(double, x)));
-#	else
-	long double g = 2.0L / x;
-	long double h = g;
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	g = g * (1.5L - (h) * g * g);
-	return g;
-#	endif
+	if (mc_isnan(x) || mc_isinf(x)) {
+		return x;
+	}
+	if (x <= 0.0L) {
+		return MCK_INF;
+	}
+	return 1.0L / x;
 }
 
 #endif /* !MC_RSQRT_H */
